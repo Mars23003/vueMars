bbb9b3a15cab7b6ddae4cad76460f46bb64f8073
mars23003 <mars8602@gmail.com>
2020-12-03 17:26:17 +0800

    12/03

--- src/components/KeyinUser.vue
+++ src/components/KeyinUser.vue
@@ -1,3 +1,45 @@
 <template>
-  <div>KeyinUser</div>
+  <v-form ref="form">
+    <v-card-text>
+      <v-text-field
+        label="輸入您的帳號"
+        type="text"
+        class="font-weight-bold"
+        outlined
+        clearable
+        v-model="myAccountId"
+        :rules="usernameRules"
+        :counter="usernameLength"
+      ></v-text-field>
+      <!-- <v-text-field v-model="myAccountId"></v-text-field> -->
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" class="mr-2">繼續</v-btn>
+    </v-card-actions>
+  </v-form>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      usernameLength: 10,
+      usernameRules: [
+        v => !!v || "請輸入帳號",
+        v => (v && v.length <= this.usernameLength) || "最多10碼"
+      ]
+    };
+  },
+  computed: {
+    myAccountId: {
+      get() {
+        return this.accountId;
+      },
+      set(value) {
+        this.$emit("update:accountId", value);
+      }
+    }
+  }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,12 +3,14 @@
     <v-row align="center" justify="center">
       <v-col cols="12" sm="8" md="4">
         <v-card class="elevation-8 pt-12 pb-12 pl-6 pr-6">
+          <v-img height="60" contain src="../assets/logo.png"></v-img>
           <v-card-title v-text="card.cardTitle" class="font-weight-black justify-center"></v-card-title>
           <v-card-subtitle v-text="card.cardSubTitle" class="font-weight-black text-center mt-0"></v-card-subtitle>
+          <!-- <router-view /> -->
+          <router-view :account-id.sync="user.accountId"></router-view>
         </v-card>
       </v-col>
     </v-row>
-    <router-view />
   </div>
 </template>
 <script>
@@ -16,10 +18,21 @@ export default {
   data() {
     return {
       card: {
-        cardTitle: "登入",
-        cardSubTitle: "請輸入密碼"
+        cardTitle: "",
+        cardSubTitle: ""
+      },
+      user: {
+        username: "",
+        accountId: "",
+        password: ""
       }
     };
+  },
+  methods: {
+    setCard(title, subTitle) {
+      this.card.cardTitle = title;
+      this.card.cardSubTitle = subTitle;
+    }
   }
 };
 </script>
