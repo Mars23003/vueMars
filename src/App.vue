67caa174dd07296911ab323651136b131b281db3
mars23003 <mars8602@gmail.com>
2020-12-09 11:30:48 +0800

    口罩地圖實作練習

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,145 @@
 <template>
-  <div>
-    <core-app-bar />
+  <div id="app">
+    <div class="row no-gutters">
+      <!-- 選擇所在區域 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="cityName" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+            <!-- <select id="cityName" class="form-control"></select> -->
+            <select id="cityName" class="form-control" v-model="select.city">
+              <option value>請選擇縣市</option>
+              <option :value="c.CityName" v-for="c in cityName" :key="c.CityName">{{ c.CityName }}</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="area" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <!-- <select id="area" class="form-control"></select> -->
+            <select id="area" class="form-control" v-model="select.area">
+              <option value>請選擇地區</option>
+              <!-- // 建立專案時若有選擇 Airbnb 相關規範設定的話
+              // 需留意過長的程式碼會造成 line length error，換行即可解決此問題-->
+              <option
+                :value="a.AreaName"
+                v-for="a in cityName.find((city) => city.CityName === select.city).AreaList"
+                :key="a.AreaName"
+              >{{ a.AreaName }}</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示藥局位置 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// 導入內部檔案
+import cityName from "./assets/cityName.json";
+
+let openStreetMap = {};
+
 export default {
   name: "App",
-
-  components: {
-    CoreAppBar: () => import("@/components/core/AppBar")
-  },
-
+  // 製作元件
   data: () => ({
-    //
-  })
+    // 空陣列 data
+    data: [],
+    cityName,
+    // select 先暫時設定為空物件
+    select: {
+      city: "桃園市",
+      area: "桃園區"
+    }
+  }),
+  computed: {
+    pharmacies() {
+      return this.data.filter(pharmacy => {
+        if (!this.select.area) {
+          return pharmacy.properties.county === this.select.city;
+        }
+        return pharmacy.properties.town === this.select.area;
+      }
+    }
+  },
+  watch: {
+    pharmacies(value) {
+      this.updateMap();
+      console.log(value);
+    }
+  },
+  methods: {
+    updateMap() {
+      // clear markers
+      openStreetMap.eachLayer(layer => {
+        if (layer instanceof L.Marker) {
+          openStreetMap.removeLayer(layer);
+        }
+      });
+      this.pharmacies.forEach(pharmacy => {
+        // 透過藥局經緯度疊加標記
+        L.marker([
+          pharmacy.geometry.coordinates[1],
+          pharmacy.geometry.coordinates[0]
+        ]).addTo(openStreetMap)
+          .bindPopup(`<p><strong style="font-size: 20px;">${
+          pharmacy.properties.name
+        }</strong></p>
+          <strong style="font-size: 16px; color: #d45345;">口罩剩餘：成人 - ${
+            pharmacy.properties.mask_adult
+              ? `${pharmacy.properties.mask_adult} 個`
+              : "未取得資料"
+          } / 兒童 - ${
+          pharmacy.properties.mask_child
+            ? `${pharmacy.properties.mask_child} 個`
+            : "未取得資料"
+        }</strong><br>
+          地址: ${pharmacy.properties.address}<br>
+          電話: ${pharmacy.properties.phone}<br>
+          <small>最後更新時間: ${pharmacy.properties.updated}</small>`);
+      });
+    }
+  },
+  mounted() {
+    const api =
+      "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
+    this.$http.get(api).then(response => {
+      // 將空陣列 data 指定為遠端獲得的資料，資料僅需取得藥局資訊即可
+      this.data = response.data.features;
+      // 將結果印出來看看
+      console.log(response);
+    });
+
+    openStreetMap = L.map("map", {
+      center: [25.042474, 121.513729],
+      // 可以嘗試改變 zoom 的數值
+      // 筆者嘗試後覺得 18 的縮放比例是較適當的查詢範圍
+      zoom: 18
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 20
+    }).addTo(openStreetMap);
+  }
+  // ...
 };
 </script>
+
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+
+#map {
+  position: relative;
+  height: 100vh;
+}
+</style>
 
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import L from 'leaflet';
+
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios);
+Vue.use(L);
 
 new Vue({
   router,
